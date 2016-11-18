5da0164697283dc2adaa500cfb650c4beac2ff7a
kots <kots___@mail.ru>
2016-11-18 17:18:04 +0200

    18.11.2016

--- src/com/javarush/test/level04/lesson16/home03/Solution.java
+++ src/com/javarush/test/level04/lesson16/home03/Solution.java
@@ -10,6 +10,33 @@ public class Solution
 {
     public static void main(String[] args)   throws Exception
     {
+        //напишите тут ваш код
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum=0;
+
+        int a;
+
+        for (int i=0; i<1000;i++)
+        {
+            String sa = r.readLine();
+            a = Integer.parseInt(sa);
+
+            if (a!=-1)
+            {
+
+                sum+=a;
+
+            }
+            else
+            {
+
+                System.out.println(sum += a);
+                break;
+            }
+
+        }
 
     }
 }
