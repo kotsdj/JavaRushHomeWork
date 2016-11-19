1c859e70e8fec4d89ec1a3e5d4803c172d7298d0
kots <kots___@mail.ru>
2016-11-19 16:55:34 +0200

    19.11.16

--- src/com/javarush/test/level04/lesson06/task03/Solution.java
+++ src/com/javarush/test/level04/lesson06/task03/Solution.java
@@ -5,27 +5,39 @@ package com.javarush.test.level04.lesson06.task03;
 */
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 
         int a = Integer.parseInt(r.readLine());
         int b = Integer.parseInt(r.readLine());
         int c = Integer.parseInt(r.readLine());
 
-        if (a>b || a>c)
+        if ((a>b)&&(a>c)){
+            if (b>c) System.out.println(a+" "+b+" "+c);
+            else System.out.println(a+" "+c+" "+b);
+        }
+
+        if ((b>a)&&(b>c)){
+            if (a>c) System.out.println(b+" "+a+" "+c);
+            else System.out.println(b+" "+c+" "+a);
+        }
+
+        if ((c>a)&&(c>b)){
+            if (a>b) System.out.println(c+" "+a+" "+b);
+            else System.out.println(c+" "+b+" "+a);
+        }
 
 
-
-
-
-
-            System.out.println(a);
+        for (int i=0; i<list.size();i++){
+            System.out.println(list.get(i));
+        }
 
     }
 }
